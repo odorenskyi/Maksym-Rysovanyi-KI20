819ff2aad3a57076ddc09c274d6fa2be4dd9e441
BagsBoom <maxcimofficial@gmail.com>
2021-04-02 13:17:59 +0300

    Added validation to chars and nums

--- lab08/prj/Рисований_task/main.cpp
+++ lab08/prj/Рисований_task/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "ModulesRisovany.h"
 #include <windows.h>
+#include <math.h>
+#include <ctype.h>
 
 using namespace std;
 
@@ -10,9 +12,9 @@ void copyright()
 }
 void expression(char a, char b, bool result)
 {
-    cout << "”ведiть a: ";
+    cout << "”ведiть символ a: ";
     cin >> a;
-    cout << "”ведiть b: ";
+    cout << "”ведiть символ b: ";
     cin >> b;
     if(a <= b - 32)
     {
@@ -21,21 +23,37 @@ void expression(char a, char b, bool result)
     else{
         result = 0;
     }
-    cout << a << endl << b << endl;
+    // cout << a << endl << b << endl;
     cout << "Result: " << result;
 }
 
-void func(int x, int y, int res)
+void func(float x, float y, float res)
 {
     cout << "”ведiть число x: ";
     cin >> x;
+    if(x < 0 || cin.fail())
+    {
+        cout << "”ведiть цiле натуральне число!" << endl;
+        system("pause");
+        getchar();
+        getchar();
+        exit(0);
+    }
     cout << "”ведiть число y: ";
     cin >> y;
-    cout << "„исла x та y у шiстнадц€ков≥й системi численн€: " << hex << x << "  " << hex << y << endl;
-    cout << "„исла x та y у дес€тковiй системi численн€: " << dec << x << "  " << dec << y << endl;
+    if(y < 3.15 || cin.fail())
+    {
+        cout << "”ведiть цiле натуральне число та €ке бiльше числа PI!" << endl;
+        system("pause");
+        getchar();
+        getchar();
+        exit(0);
+    }
+    cout << "„исла x та y у шiстнадц€ковiй системi численн€: " << hexfloat << x << "  " << hexfloat << y << endl;
+    cout << "„исла x та y у дес€тковiй системi численн€: " << defaultfloat << x << "  " << defaultfloat << y << endl;
     res = s_calculation(x,y);
-    cout << "–езультат у дес€тковiй cистемi: " << dec << res << endl;
-    cout << "–езультат у ш≥стнадц€тковiй системi: " << hex << res << endl;
+    cout << "–езультат у дес€тковiй cистемi: " << defaultfloat << res << endl;
+    cout << "–езультат у шiстнадц€тковiй системi: " << hexfloat << res << endl;
 }
 
 int main()
